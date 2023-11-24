d7e8fb294512058ad97f95457db1151a47c91336
Bjarki Badda <bbaldvinsson@gmail.com>
2023-11-24 01:42:22 +0000

    adding functionality after review

--- calc.js
+++ calc.js
@@ -1,18 +1,25 @@
-// Constants for costs
-const hourGeneralCost = 4278;
+// Constants
+const HOUR_GENERAL_COST = 4278;
 const breakfastCost = 2908;
 const lunchCost = 6925;
 const afternoonSnackCost = 2908;
 
-// Hourly costs
-const hourCosts = {
+const hoursCost = {
   4: 17114,
   5: 21392,
   6: 25671,
   7: 29949,
   8: 34228,
-  8.5: 38506,
 };
+
+//breytur
+var childrenCount
+var hours
+var secondHours = 0
+var totalCost
+var discountPercentage
+var timeCost
+var costForNt
 
 document.addEventListener('DOMContentLoaded', function() {
   const singleBtn = document.getElementById('single');
@@ -40,170 +47,113 @@ document.addEventListener('DOMContentLoaded', function() {
     isSingle = true;
     singleBtn.classList.add('active');
     coupleBtn.classList.remove('active');
+    salaryLabel.textContent = isSingle ? "Tekjubil fyrir einstæða forelda" : "Tekjubil fyrir par";
   });
 
   coupleBtn.addEventListener('click', function() {
     isSingle = false;
     coupleBtn.classList.add('active');
     singleBtn.classList.remove('active');
+    salaryLabel.textContent = isSingle ? "Tekjubil fyrir einstæða forelda" : "Tekjubil fyrir par";
   });
 
-  const salarySelect = document.getElementById('salaryRange');
   const salaryLabel = document.getElementById('salaryLabel');
 
-  function updateSalaryOptions(isSingle) {
-    while (salarySelect.firstChild) {
-      salarySelect.removeChild(salarySelect.firstChild);
-    }
-
-    const options = isSingle ? getSingleSalaryOptions() : getCoupleSalaryOptions();
-    options.forEach((option) => {
-      const optionElement = document.createElement('option');
-      optionElement.value = option.value;
-      optionElement.textContent = option.text;
-      salarySelect.appendChild(optionElement);
+    document.getElementById('single').addEventListener('click', function() {
+      document.getElementById('singleSalary').style.display = 'block';
+      document.getElementById('coupleSalary').style.display = 'none';
+      isSingle = true;
+      salaryLabel.textContent = "Tekjubil fyrir einstæða forelda"
     });
+    
+    document.getElementById('couple').addEventListener('click', function() {
+      document.getElementById('singleSalary').style.display = 'none';
+      document.getElementById('coupleSalary').style.display = 'block';
+      isSingle = false;
+      salaryLabel.textContent = "Tekjubil fyrir par";
 
-    salaryLabel.textContent = isSingle ? "Tekjubil fyrir einstæða forelda" : "Tekjubil fyrir par";
-  }
-
+    });
 
   singleBtn.addEventListener('click', function() {
     isSingle = true;
     singleBtn.classList.add('active');
     coupleBtn.classList.remove('active');
-    updateSalaryOptions(isSingle);
+
   });
 
   coupleBtn.addEventListener('click', function() {
     isSingle = false;
     coupleBtn.classList.add('active');
     singleBtn.classList.remove('active');
-    updateSalaryOptions(isSingle);
+
   });
 
-  // Initialize the salary options based on the default isSingle value
-  updateSalaryOptions(isSingle);
-
-
   document.getElementById('calculate').addEventListener('click', function() {
-    const childrenCount = parseInt(document.getElementById('childrenCount').value, 10);
-    const salarySelect = document.getElementById('salaryRange');
-    const salary = salarySelect.options[salarySelect.selectedIndex].value;
-    const hours = parseFloat(document.getElementById('hours').value);
+    ///todo: add validation to the fields;
 
-    // Validate inputs
-    if (isNaN(childrenCount) || isNaN(salary) || isNaN(hours)) {
-      alert('Please enter valid numbers for all fields.');
-      return;
+    childrenCount = Number(document.getElementById('childrenCount').value);
+    discountPercentage = isSingle ? Number(document.getElementById('singleSalary').value): Number(document.getElementById('coupleSalary').value);
+    hours = Number(document.getElementById('hours').value);
+    var timeCostWithoutDiscount;
+    
+    //reikna systkinaafslátt
+    if(childrenCount === 0 || hours === 0){
+      totalCost = 0;
     }
 
-    try {
-      const totalCost = calculateTotalCost(hours, childrenCount, salary, isSingle);
+    //tímakostnaður fyrir fyrsta barn
+    timeCost = hoursCost[hours];
+    timeCostWithoutDiscount = timeCost;
+  
+
+    if(childrenCount > 1){
+      // kostnaður við annað barn
+      secondHours = Number(document.getElementById('additionalHours').value); 
+      // 50% afsláttur á barni númer tvö
+      timeCost += hoursCost[secondHours]*0.5;
       
+      //bæti við tímagjaldi á barn nr tvö miðað við innslegnar forsendur. Börn umfram það reiknaðar m.v. fyrsta barn.
+      timeCostWithoutDiscount +=hoursCost[secondHours] + hoursCost[hours]*(childrenCount-2);
+    }
+
+    //reikna afslátt vegna launa
+    timeCost = timeCost * (1-discountPercentage);
+
+    //reikna matarkostnað.
+    var foodCost = (breakfastCost + lunchCost)*childrenCount
+    
+    if(hours >=7){
+      foodCost +=afternoonSnackCost;
+    }
+
+     if(childrenCount === 2 && secondHours >=7 ){
+      foodCost +=afternoonSnackCost;
+    }
+
+    // ef börnin eru fleiri en tvö, þá gerum við ráð fyrir að börn 3 og 4 séu jafn lengi og barn eitt á leikskólanum.
+    else if(childrenCount > 2 && hours >= 7){
+      foodCost += afternoonSnackCost *(childrenCount-2);
+    }
+
+    totalCost = timeCost + foodCost;
+
+    costForNt = timeCostWithoutDiscount/0.1082;
+  
+
+    try {
       const formattedTotalCost = formatCurrency(totalCost);
       document.getElementById('totalCost').textContent = formattedTotalCost;
       document.getElementById('total').textContent = formattedTotalCost + " kr.";
       document.getElementById('detailsGrid').style.display = 'grid';
+      document.getElementById('incomeDiscount').textContent = discountPercentage * 100 + '%';
+      document.getElementById('foodFee').textContent = formatCurrency(foodCost) + ' kr.';
+      document.getElementById('schoolFee').textContent = formatCurrency(timeCost) + ' kr.';
+      document.getElementById('ntTotal').textContent = formatCurrency(costForNt) + ' kr.';
+
     } catch (error) {
       alert(error.message);
     }
-  });
 });
-
-function getSingleSalaryOptions() {
-  return [
-    { text: "0 - 430.999", value: "430999" },
-    { text: "431.000 - 550.999", value: "550999" },
-    { text: "551.000 - 670.999", value: "670999" },
-    { text: "> 671.000", value: "671000" },
- 
-  ];
-}
-
-function getCoupleSalaryOptions() {
-  return [
-    { text: "0 - 689.599", value: "689599" },
-    { text: "689.600 - 881.599", value: "881599" },
-    { text: "881.600 - 1.073.599", value: "1073599" },
-    { text: "> 1.073.600", value: "1073600" },
-  ];
-}
-
-// Function to calculate the total cost
-function calculateTotalCost(hours, childrenCount, salary, isSingle) {
-  // Calculate base cost with sibling discount
-  let cost = getCostWithSiblingsDiscount(hours, childrenCount);
-
-  // Apply salary-based discount
-  const discountPercentage = getDiscountPercentage(salary, isSingle);
-  let hoursCost = cost * (1 - discountPercentage);
-  document.getElementById('schoolFee').textContent = formatCurrency(hoursCost) + ' kr.';
-
-  // Add food costs
-  const totalCost = getCostWithFoodCost(hoursCost, hours, childrenCount);
-
-  return totalCost;
-}
-
-// Function to get the hourly cost with siblings discount
-function getCostWithSiblingsDiscount(hours, children) {
-  let hoursCost = hourCosts[hours] || hourGeneralCost;
-
-  // Apply siblings discount
-  if (children > 1) {
-    hoursCost += hoursCost * 0.5; // 50% discount for the second child
-  }
-
-  return hoursCost;
-}
-
-// Function to get the discount percentage based on salary
-function getDiscountPercentage(salary, isSingle) {
-  // Convert salary string to number for comparison
-  salary = Number(salary);
-  const discountPercentage =  isSingle ? getSingleSalaryCategory(salary) : getCoupleSalaryCategory(salary);
-  document.getElementById('incomeDiscount').textContent = discountPercentage * 100 + '%';
-  return discountPercentage;
-}
-
-// Functions to get discount percentage based on single and couple salary categories
-function getSingleSalaryCategory(salary) {
-  if (salary <= 430999) {
-    return 0.75;
-  } else if (salary <= 550999) {
-    return 0.50;
-  } else if (salary <= 670999) {
-    return 0.25;
-  } else {
-    return 0.0; // Invalid salary will not apply discount
-  }
-}
-
-function getCoupleSalaryCategory(salary) {
-  if (salary <= 689599) {
-    return 0.75;
-  } else if (salary <= 881599) {
-    return 0.50;
-  } else if (salary <= 1073599) {
-    return 0.25;
-  } else {
-    return 0.0; // Invalid salary will not apply discount
-  }
-}
-
-// Function to calculate the total cost with food
-function getCostWithFoodCost(hoursCost, hours, childrenCount) {
-  let foodCost = (breakfastCost + lunchCost)*childrenCount;
-  // Add afternoon snack cost if hours are 7 or more
-  if (hours >= 7) {
-    foodCost += afternoonSnackCost * childrenCount;
-  }
-
-  document.getElementById('foodFee').textContent = formatCurrency(foodCost) + ' kr.';
-
-  return foodCost + hoursCost;
-}
 
 // Function to format the currency
 function formatCurrency(number) {
@@ -217,4 +167,4 @@ function formatCurrency(number) {
   let formatted = numberStr.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
 
   return formatted;
-}
+}})
